45e4b940592af75ac173f83ff91ec6fa44e85dcb
Melissa <mawilliamson10@gmail.com>
2022-01-20 02:53:29 +0000

    looks for a word within 2D array of letters

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    if (!letters.length) return false;
 
-module.exports = wordSearch
+    // check each row for word
+    const horizontalJoin = letters.map(ls => ls.join(""));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    // check each column for word
+    const verticalJoin = [];
+    for (let i = 0; i < letters.length; i++) {
+        for (let j = 0; j < letters[i].length; j++) {
+            if (i === 0) {
+                verticalJoin.push([letters[i][j]]);
+            } else {
+                verticalJoin[j].push(letters[i][j]);
+            }
+        }
+    }
+
+    let verticalJoined = verticalJoin.map(ls => ls.join(""));
+    for (l of verticalJoined) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
